54b204c3066ed10c15638f5d150d3d0da22e6893
leesr1972 <sergey.r.lee@gmail.com>
2022-10-22 15:03:58 +0300

    Это проект, реализующий телеграм-бота для приюта домашних животных. Этот проект разработан мной в составе группы из трёх человек.

---
+++ src/main/java/sky/pro/java/course6/projecthappypet/ProjectHappyPetApplication.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course6.projecthappypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectHappyPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectHappyPetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sky/pro/java/course6/projecthappypet/ProjectHappyPetApplicationTests.java
@@ -0,0 +1,13 @@
+package sky.pro.java.course6.projecthappypet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectHappyPetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
